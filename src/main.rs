8ca7a3802e85fd5907aedea853c78fbbeaea6aa8
BufferOverflow <scharxidev@outlook.de>
2023-02-03 17:07:28 +0100

    getting user input to calculate the factorial

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,21 @@ fn factorial(num: i32) -> i32 {
 }
 
 fn main() {
-    println!("{}", factorial(5));
+    let args = std::env::args().skip(1).collect::<Vec<String>>();
+
+    if args.len() > 2 {
+        eprintln!("factorial <number>");
+        std::process::exit(1);
+    }
+
+    if args[0].trim().to_lowercase() != "factorial" {
+        eprintln!("factorial <number>");
+        std::process::exit(1);
+    }
+
+    let number = args[1].trim().parse::<i32>().expect(&format!("{} is not a number", args[1]));
+
+    println!("The factorial of {number} is {}", factorial(number));
 }
 
 #[test]
